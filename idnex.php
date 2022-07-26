36a73a264b17d621a5333025a86f6a190c60f5e8
Matheus <90228238+ofmxtheuuz@users.noreply.github.com>
2022-07-26 18:27:56 -0300

    Add files via upload

---
+++ idnex.php
@@ -0,0 +1,20 @@
+<?php
+require 'vendor/autoload.php';
+
+$email = new \SendGrid\Mail\Mail();
+$email->setFrom("contato@mxtheuz.com.br", "Mxtheuz");
+$email->setSubject("Sending with SendGrid is Fun");
+$email->addTo("mategame2402@gmail.com", "Henrique Viadinho");
+$email->addContent("text/plain", "and easy to do anywhere, even with PHP");
+$email->addContent(
+    "text/html", "<strong>and easy to do anywhere, even with PHP</strong>"
+);
+$sendgrid = new \SendGrid("");
+try {
+    $response = $sendgrid->send($email);
+    print $response->statusCode() . "\n";
+    print_r($response->headers());
+    print $response->body() . "\n";
+} catch (Exception $e) {
+    echo 'Caught exception: '. $e->getMessage() ."\n";
+}
